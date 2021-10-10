9ca7d94dd781d7dc75100f3ffb58d662e0c99537
Vaibhav <vaibhavacharya111@gmail.com>
2021-10-10 09:42:57 +0530

    fix: font loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
 
 import { IdProvider } from "@radix-ui/react-id";
-// import "./../styles/reset.css";
-// import "./../styles/utilities.css";
-// import "./../styles/components.css";
-// import "./../styles/custom.css";
+
 import { globalReset } from "../stitches.config";
 
 function _App({ Component, pageProps }) {
@@ -12,19 +9,6 @@ function _App({ Component, pageProps }) {
 
   return (
     <IdProvider>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <Component {...pageProps} />
     </IdProvider>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,17 @@ export default class _Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;600&display=swap"
+            rel="stylesheet"
+          />
+
           <style
             id="stitches"
             dangerouslySetInnerHTML={{ __html: getCssText() }}
